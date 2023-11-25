cf009282cde63ee26f671ae8cf6526a43b3801a5
KazikS <90966310+KazikS@users.noreply.github.com>
2023-11-25 21:49:55 +0300

    complete main page

--- django/codolangua/codelang/views.py
+++ django/codolangua/codelang/views.py
@@ -6,3 +6,7 @@ def index(request):
     data = {'title': 'Главная страница'}
     return render(request, 'codelang/index.html', context=data)
 
+
+def python(request):
+    data = {'title': 'Теория Python'}
+    return render(request, 'codelang/python-page.html', context=data)
