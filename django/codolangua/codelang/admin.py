d6f4d0522aa5d91f6d458bb3dee65c84330b1a23
MatveyMahlyshev <matthewfootballer11@gmail.com>
2023-12-17 16:39:03 +0300

    admin

--- django/codolangua/codelang/admin.py
+++ django/codolangua/codelang/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from .models import Language, Topic
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    fields = ['topic_title', 'topic_content', 'topic_slug', 'programming_languages']
+    list_display = ['topic_title', 'topic_content', 'topic_slug', 'get_programming_languages']
+    list_display_links = ['topic_title']
+    filter_vertical = ('programming_languages',)
+    ordering = ['programming_languages']
+
+    def get_programming_languages(self, obj):
+        return ", ".join([p.title for p in obj.programming_languages.all()])
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['title']
