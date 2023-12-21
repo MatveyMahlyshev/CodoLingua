b9d4f624084b604202293147bb476e116f01afdb
MatveyMahlyshev <matthewfootballer11@gmail.com>
2023-12-21 08:53:24 +0300

    &nbsp

--- django/codolangua/codelang/urls.py
+++ django/codolangua/codelang/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('js/', views.show_js_page, name='js'),
     path('html-css/', views.show_html_css_page, name='html-css'),
     path('python/<slug:python_slug>/', views.show_topic_page, name='python_topic'),
-    path('registration/', views.show_registration_page, name='registration'),
 ]
--- django/codolangua/codelang/views.py
+++ django/codolangua/codelang/views.py
@@ -14,10 +14,6 @@ def index(request):
 def show_python_page(request):
     language = Language.objects.get(title='Python')
     topics = Topic.objects.filter(programming_languages=language)
-    # for i in Topic.objects.filter(pk__gt=35):
-    #     language.topic.add(i)
-    i = 1
-    
     data = {'title': f'Теория {language.title}',
             'topics': topics}
     return render(request, 'codelang/python-page.html', context=data)
@@ -45,31 +41,13 @@ def show_topic_page(request, python_slug):
     topic = get_object_or_404(Topic, topic_slug=python_slug)
     t = {}
     i = 1
-    for paragraphs in topic.topic_text.splitlines():
+    for paragraphs in topic.topic_text.split('&nbsp'):
         t[f'paragraph{i}'] = paragraphs
         i += 1
     print(t)
-
-    if python_slug == 'introduction-to-python':
-        data = {'title': topic.topic_title,
-                'topics': topic,
-                'text': t,
-                }
-    else:
-        data = {'title': topic.topic_title,
-                'topics': topic,
-                'text': t,
-                }
+    data = {'title': topic.topic_title,
+            'topics': topic,
+            'text': t,
+            }
 
     return render(request, 'codelang/topic-page.html', context=data)
-
-
-def show_registration_page(request):
-    data = {'title': 'Регистрация'}
-    return render(request, 'codelang/reg_form.html', context=data)
-
-
-def authorization(request):
-    data = {'title': 'Главная страница'}
-    return render(request, 'codelang/index.html', context=data)
-
